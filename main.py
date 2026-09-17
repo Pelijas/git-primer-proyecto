a8637b12c44c1c586de9be5d0a6aaf64fc3c7a7e
Pelijas <martinpelijas@gmail.com>
2026-09-17 19:58:54 +0200

    Añado mensaje

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ edad = input("¿Cuántos años tienes? ")
 print(f"Tienes {edad} años.")
 
 print("¡Bienvenido a mi primer proyecto Git!")
+
+print("Esta línea se ha añadido desde el proyecto original.")
+print("Esta línea se ha añadido desde el proyecto original.")
